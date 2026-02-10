5c7f22c3530c5be37fa060b570d6014bbe2ea05a
Waldemberg <waldemberg.pereira@outlook.com>
2026-02-10 11:35:20 -0300

    Configurado a raiz da url para redirecionar o usuário para o login

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='login', permanent=False)),
     path('admin/', admin.site.urls),
     path('servicos/', include('servicos.urls')),
-    path('core/', include('core.urls'))
+    path('core/', include('core.urls')),
 ]
